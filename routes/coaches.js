de723aa92d569f06da0bd809dd46cfb32c0243fc
Santiago Aguilar <santiago2000aguilar@gmail.com>
2019-02-26 01:58:46 +0000

    create route and image link to show page for players and coaches

--- routes/coaches.js
+++ routes/coaches.js
@@ -10,9 +10,23 @@ router.get("/",function(req,res){
       var coaches=result;
         //  res.send("You've reached the home page. There are "+count+" users");
          console.log(coaches);
-        res.render("coaches",{coaches:coaches});
+        res.render("coaches/coaches",{coaches:coaches});
 
     });
 });
+
+router.get("/:id", function(req, res){
+    var id=req.params.id;
+    var q="SELECT * FROM persons JOIN coaches ON persons.id = coaches.person_id WHERE persons.id ="+id+" ;";
+     connection.query(q,function(err, result) {
+      if (err) throw err;
+      var foundCoach=result[0];
+        //  res.send("You've reached the home page. There are "+count+" users");
+         console.log(foundCoach);
+        res.render("coaches/show",{coach:foundCoach});
+
+    });
+});
+
 
 module.exports = router;//exports routes to main app.js file
--- routes/players.js
+++ routes/players.js
@@ -12,7 +12,20 @@ router.get("/",function(req,res){
       var players=result;
         //  res.send("You've reached the home page. There are "+count+" users");
          console.log(players);
-        res.render("players",{players:players});
+        res.render("players/players",{players:players});
+
+    });
+});
+
+router.get("/:id", function(req, res){
+    var id=req.params.id;
+    var q="SELECT * FROM persons JOIN athletes ON persons.id = athletes.person_id LEFT JOIN students ON persons.id = students.person_id WHERE persons.id ="+id+" ;";
+     connection.query(q,function(err, result) {
+      if (err) throw err;
+      var foundPlayer=result[0];
+        //  res.send("You've reached the home page. There are "+count+" users");
+         console.log(foundPlayer);
+        res.render("players/show",{player:foundPlayer});
 
     });
 });
